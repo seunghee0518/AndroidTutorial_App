f9a3b5d4d244b3400444422522686956f9322bfc
seunghee <shee6185@gmail.com>
2021-02-19 00:57:46 +0900

    6면 주사위 굴리기

--- app/src/main/java/com/example/happybirthday/MainActivity_Dice.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity_Dice.kt
@@ -13,15 +13,32 @@ class MainActivity_Dice : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main__dice)
 
+        //굴리기 버튼
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView : TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+        rollButton.setOnClickListener { rollDice() }
 
-        val back : Button = findViewById(R.id.btnBack)
+        //되돌아가기 버튼
+        val back: Button = findViewById(R.id.btnBack)
         back.setOnClickListener {
             finish()
         }
+    }
+    //주사위 굴리고, 그 결과를 스크린에 업데이터함
+    private fun rollDice() {
+        //새로운 6면 주사위를 만들고 굴리기
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        //주사위굴린 화면을 업데이트함함
+       val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+//클래스 Dice 입력
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
