9d1dbbbf9b3d2ee8fe95825745aa487419f68fbb
seunghee <shee6185@gmail.com>
2021-02-19 21:50:47 +0900

    조건문 삽입

--- app/src/main/java/com/example/happybirthday/MainActivity_Dice.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity_Dice.kt
@@ -40,3 +40,15 @@ class Dice(val numSides: Int) {
         return (1..numSides).random()
     }
 }
+
+fun main () {
+    val num = 4
+    if (num > 4) {
+        println("The variable is greater than 4")
+    } else if (num == 4) {
+        println("The variable is equal to 4")
+    } else {
+        println("The variable is less than 4")
+        }
+    }
+}
