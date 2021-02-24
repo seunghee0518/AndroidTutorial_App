71c8bdcb785e2135263228bae771da42058c50bc
seunghee <shee6185@gmail.com>
2021-02-25 00:15:22 +0900

    팁계산기 레이아웃 생성

---
+++ app/src/main/java/com/example/AndroidTutorial_App/MainActivity_TipCalculator.kt
@@ -0,0 +1,11 @@
+package com.example.AndroidTutorial_App
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_TipCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main__tip_calculator)
+    }
+}
