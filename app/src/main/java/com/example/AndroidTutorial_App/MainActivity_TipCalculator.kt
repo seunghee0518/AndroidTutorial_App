95a4b6e67d21e624a622934ad6c725593a2a1b44
seunghee <shee6185@gmail.com>
2021-03-02 22:25:23 +0900

    뷰바인딩 활성화

--- app/src/main/java/com/example/AndroidTutorial_App/MainActivity_TipCalculator.kt
+++ app/src/main/java/com/example/AndroidTutorial_App/MainActivity_TipCalculator.kt
@@ -4,11 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.AndroidTutorial_App.databinding.ActivityMainBinding
 
 class MainActivity_TipCalculator : AppCompatActivity() {
+    //뷰바인딩을 사용하는 코드
+    //바인딩개체에 대한 클래스의 최상위 변수를 선언함
+    //lateinit: 코드가 변수를 사용하기전 초기화함, 그렇지 않으면 앱이 충돌함
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main__tip_calculator)
+        //binding을 액세스하는데 사용할 개체를 초기화함
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //활동의 콘텐츠보기를 설정함, 레이아웃의 리소스 id를 전달하는대신 뷰계층구조의 루트를 지정함
+        setContentView(binding.root)
 
         //되돌아가기 버튼
         val BackToBirthday = Intent(this, MainActivity::class.java)
